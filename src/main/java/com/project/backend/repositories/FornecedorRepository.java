18013bd817ea1fbd785c2e99b1175f3e939b07ec
lsiin <vitorantonio-7@hotmail.com>
2025-03-09 18:12:14 -0300

    First commit

--- src/main/java/com/project/backend/repositories/FornecedorRepository.java
+++
@@ -1,8 +0,0 @@
-package com.project.backend.repositories;
-
-import com.project.backend.entities.Fornecedor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FornecedorRepository extends JpaRepository<Fornecedor, Long> {
-
-}
