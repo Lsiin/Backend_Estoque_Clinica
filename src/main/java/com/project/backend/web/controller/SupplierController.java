4c918611aa19a83d9b34492560d07e383ff436f2
lsiin <vitorantonio-7@hotmail.com>
2025-03-18 16:59:50 -0300

    Add: Add swagger to application

--- src/main/java/com/project/backend/web/controller/SupplierController.java
+++ src/main/java/com/project/backend/web/controller/SupplierController.java
@@ -2,6 +2,10 @@ package com.project.backend.web.controller;
 
 import com.project.backend.entities.Supplier;
 import com.project.backend.services.SupplierServices;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,7 +24,15 @@ public class SupplierController {
 
 
 
-
+    @Operation(summary = "Register a supplier",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Supplier registration successfully",
+                            content = @Content(mediaType = "application/Json", schema = @Schema(implementation = Supplier.class))),
+                    @ApiResponse(responseCode = "400", description = "Could not create supplier",
+                            content = @Content(mediaType = "application/Json", schema = @Schema(implementation = ErrorResponses.class))),
+                    @ApiResponse(responseCode = "500", description = "Internal error",
+                            content = @Content(mediaType = "application/Json", schema = @Schema(implementation = ErrorResponses.class))),
+            })
     @PostMapping("/register")
     public ResponseEntity<Supplier> cadastrarFornecedor(@RequestBody Supplier supplier) {
         Supplier savedSupplier = supplierServices.registerSupplier(supplier);
@@ -37,6 +49,13 @@ public class SupplierController {
     }
 
 
+    @Operation(summary = "Find supplier by ID",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Supplier found",
+                            content = @Content(mediaType = "application/Json", schema = @Schema(implementation = Supplier.class))),
+                    @ApiResponse(responseCode = "404", description = "Supplier not found",
+                            content = @Content(mediaType = "application/Json", schema = @Schema(implementation = ErrorResponses.class)))
+            })
     @GetMapping("/get/{id}")
     public ResponseEntity<Supplier> obterFornecedorPorId(@PathVariable Long id) {
             Optional<Supplier> supplier = supplierServices.getFornecedorById(id);
@@ -44,7 +63,13 @@ public class SupplierController {
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-
+    @Operation(summary = "Update a supplier",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Supplier updated successfully",
+                            content = @Content(mediaType = "application/Json", schema = @Schema(implementation = Supplier.class))),
+                    @ApiResponse(responseCode = "404", description = "Supplier not found",
+                            content = @Content(mediaType = "application/Json", schema = @Schema(implementation = ErrorResponses.class)))
+            })
     @PutMapping("/update/{id}")
     public ResponseEntity<Supplier> updateFornecedor(@PathVariable Long id, @RequestBody Supplier supplier) {
         Optional<Supplier> existSupplier = supplierServices.getFornecedorById(id);
@@ -57,7 +82,12 @@ public class SupplierController {
         }
     }
 
-
+    @Operation(summary = "Delete a supplier",
+            responses = {
+                    @ApiResponse(responseCode = "204", description = "Supplier deleted successfully"),
+                    @ApiResponse(responseCode = "404", description = "Supplier not found",
+                            content = @Content(mediaType = "application/Json", schema = @Schema(implementation = ErrorResponses.class)))
+            })
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<Void> excluirFornecedor(@PathVariable Long id) {
         Optional<Supplier> existSupplier = supplierServices.getFornecedorById(id);
--- src/main/java/com/project/backend/web/controller/UserController.java
+++ src/main/java/com/project/backend/web/controller/UserController.java
@@ -4,6 +4,11 @@ package com.project.backend.web.controller;
 import com.project.backend.entities.User;
 import com.project.backend.exceptions.GlobalExceptionHandler;
 import com.project.backend.repositories.UserRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,7 +19,7 @@ import java.util.Objects;
 import java.util.Optional;
 
 
-
+@Tag(name = "user", description = "Contains any registers, searches, updates and deletes, of the users ")
 @RestController
 @RequestMapping("user")
 public class UserController {
@@ -22,11 +27,15 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    @GetMapping("/test")
-    public ResponseEntity<String> testEndpoint() {
-        return ResponseEntity.ok("Working");
-    }
-
+    @Operation(summary = "Register a user",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "User registration successfully",
+                            content = @Content(mediaType = "application/Json", schema = @Schema(implementation = User.class))),
+                    @ApiResponse(responseCode = "400", description = "Could not create user",
+                            content = @Content(mediaType = "application/Json", schema = @Schema(implementation = ErrorResponses.class))),
+                    @ApiResponse(responseCode = "500", description = "Internal error",
+                            content = @Content(mediaType = "application/Json", schema = @Schema(implementation = ErrorResponses.class))),
+            })
     @PostMapping("/create")
     public ResponseEntity<?> create(@RequestBody @Valid User user, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
@@ -65,7 +74,15 @@ public class UserController {
         User savedUser = userRepository.save(user);
         return ResponseEntity.ok(savedUser);
     }
-
+    @Operation(summary = "Find user by id",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "User found",
+                            content = @Content(mediaType = "application/Json", schema = @Schema(implementation = User.class))),
+                    @ApiResponse(responseCode = "404", description = "User not found",
+                            content = @Content(mediaType = "application/Json", schema = @Schema(implementation = ErrorResponses.class))),
+                    @ApiResponse(responseCode = "500", description = "Internal error",
+                            content = @Content(mediaType = "application/Json", schema = @Schema(implementation = ErrorResponses.class))),
+            })
     @GetMapping("/get/{id}")
     public ResponseEntity<User> getUserById(@PathVariable Long id) {
         Optional<User> user = userRepository.findById(id);
@@ -76,6 +93,17 @@ public class UserController {
         }
     }
 
+    @Operation(summary = "Update a user",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "User updated successfully",
+                            content = @Content(mediaType = "application/Json", schema = @Schema(implementation = User.class))),
+                    @ApiResponse(responseCode = "400", description = "Could not update the user",
+                            content = @Content(mediaType = "application/Json", schema = @Schema(implementation = ErrorResponses.class))),
+                    @ApiResponse(responseCode = "404", description = "User not found",
+                            content = @Content(mediaType = "application/Json", schema = @Schema(implementation = ErrorResponses.class))),
+                    @ApiResponse(responseCode = "500", description = "Internal error",
+                            content = @Content(mediaType = "application/Json", schema = @Schema(implementation = ErrorResponses.class))),
+            })
     @PutMapping("/update/{id}")
     public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User userDetails) {
         return userRepository.findById(id)
@@ -95,6 +123,15 @@ public class UserController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
+    @Operation(summary = "Delete a User",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "User deleted successfully",
+                            content = @Content(mediaType = "application/Json", schema = @Schema(implementation = User.class))),
+                    @ApiResponse(responseCode = "404", description = "User not found",
+                            content = @Content(mediaType = "application/Json", schema = @Schema(implementation = ErrorResponses.class))),
+                    @ApiResponse(responseCode = "500", description = "Internal error",
+                            content = @Content(mediaType = "application/Json", schema = @Schema(implementation = ErrorResponses.class))),
+            })
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<?> deleteUser(@PathVariable Long id) {
         Optional<User> user = userRepository.findById(id);
